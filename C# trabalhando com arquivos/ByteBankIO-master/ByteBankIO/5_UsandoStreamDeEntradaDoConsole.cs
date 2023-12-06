2a7ba1cea975a30fc483a420b7eb516ee298394d
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2023-12-06 00:05:04 -0300

    Add files via upload
    Aqui estão estudos de C# onde pude aprender a ler e a criar arquivos de texto com informações

---
+++ C# trabalhando com arquivos/ByteBankIO-master/ByteBankIO/5_UsandoStreamDeEntradaDoConsole.cs
@@ -0,0 +1,28 @@
+﻿using ByteBankIO;
+using System.Diagnostics;
+using System.Text;
+
+partial class Program
+{
+    static void UsarStreamDeEntrada()
+    {
+        using (var fluxoDeEntrada = Console.OpenStandardInput())
+        using(var fs = new FileStream("entradaConsole.txt", FileMode.Create))
+        {
+            var buffer = new byte[1024];
+
+            
+
+            while (true)
+            {
+                var byteslidos = fluxoDeEntrada.Read(buffer, 0, buffer.Length);
+                fs.Write(buffer, 0 , byteslidos);
+                fs.Flush();
+
+                Console.WriteLine($"Bytes lidos no console: {byteslidos}");
+            }
+
+            
+        }
+    }
+}
---
+++ C# trabalhando com arquivos/ByteBankIO-master/ByteBankIO/6_CriandoArquivoUsandoFile.cs
@@ -0,0 +1,16 @@
+﻿using ByteBankIO;
+using System.Diagnostics;
+using System.Text;
+
+partial class Program
+{
+    static void UsandoFile()
+    {
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui: {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendocomFIle.txt", "escrevendo qualquer coisaaaaaaaaaaaaaaaa");
+        Console.WriteLine("Aplicação Finalizada !");
+        Console.ReadLine();
+    }
+}
